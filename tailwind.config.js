b336d663a944204c243285ad3a6c9d247ded2163
vijaypatil2003 <vijayptl0106@gmail.com>
2026-07-04 19:48:11 +0530

    chore: cleanup boilerplate, configure tailwind, add Space Grotesk font, update App.jsx structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Space Grotesk", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
